9f4a379a6faa22d08285c45a4645524a5a53c4e2
HyeonWooJo <HyeonWooJo@github.com>
2022-07-11 20:40:09 +0900

    Add: tests.py

--- greeneeds/users/tests.py
+++ greeneeds/users/tests.py
@@ -1,9 +1,11 @@
 import email
-import json
+from email.quoprimime import body_check
+import json, jwt
 from urllib import response
 from wsgiref import headers
 
 from django.test import TestCase, Client
+from django.conf import settings
 
 from .models        import User
 from unittest.mock  import patch, MagicMock
@@ -15,34 +17,13 @@ class KakaoSigninTest(TestCase):
             email      = "chaduri7913@naver.com",
             nickname   = "조현우"
         )
+        pass
     def tearDown(self):
         User.objects.all().delete()
 
     @patch("users.views.requests")
     def test_success_kakao_signinView_new_user(self, mocked_requests):
         client   = Client()
-
-        class KakaoResponse:
-            def json(self):
-                return {
-                        "id" : 2329972136,
-                        "kakao_account" : {
-                            "email" : "chaduri7913@naver.com"
-                        },
-                        "properties" :{
-                            "nickname" : "조현우"
-                        }
-                    }
-                
-        mocked_requests.get = MagicMock(return_value = KakaoResponse())
-        headers             = {"HTTP_Authorization" : "asdfsc123asd.asdf123dacvsdfg"}
-        response            = client.get("/signin/kakao", **headers)
-
-        self.assertEqual(response.status_code, 201)
-                
-    @patch("users.views.requests")
-    def test_fail_KakaoSigninView_post_invalid_keys(self, mocked_requests):
-        client = Client()
 
         class MockedResponse:
             def json(self):
@@ -57,7 +38,35 @@ class KakaoSigninTest(TestCase):
                     }
                 
         mocked_requests.get = MagicMock(return_value = MockedResponse())
-        headers             = {"Authorization" : "asdfsc123asd.asdf123dacvsdfg"}
-        response            = client.get("/signin/kakao", **headers)
+        # headers             = {"HTTP_Authorization" : "asdfsc123asd.asdf123dacvsdfg"}
+        body                = {"code" : "asdf112"}
+        response            = client.post("/signin/kakao", data=body)
 
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'token': response.json()['token']})
+
+                
+    @patch("users.views.requests")
+    def test_fail_kakao_signinView_post_invalid_keys(self, mocked_requests):
+        client   = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                        "id" : 2329972136,
+                        "kakao_account" : {
+                            "email" : "chaduri7913@naver.com"
+                        },
+                        "properties" :{
+                            "nickname" : "조현우"
+                        }
+                    }
+                
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        # headers             = {"HTTP_Authorization" : "asdfsc123asd.asdf123dacvsdfg"}
+        body                = {"code1" : "asdf112"}
+        response            = client.post("/signin/kakao", data=body)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'token': response.json()['token']})
+
